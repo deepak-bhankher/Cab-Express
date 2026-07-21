76be579bb148289a48b983a87d1b8d391874247e
Deepak <deepakbhankhor758@gmail.com>
2026-07-21 13:38:17 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Home from './pages/Home'
+import Navbar from './Components/Navbar'
+import Home from './Pages/Home'
 
 const App = () => {
   return (
