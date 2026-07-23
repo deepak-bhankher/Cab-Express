f221acce7f4c0dad728229a13698bfdb0ddf4ccc
Deepak <deepakbhankhor758@gmail.com>
2026-07-23 11:08:51 +0530

    file change scrolltoptop

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import ScrollToTop from './ScrollToTop'
+import ScrollToTop from './Components/Scrolltotop'
 import Navbar from './Components/Navbar'
 import Home from './Pages/Home'
 import About from './Pages/About'
